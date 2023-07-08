5155daabba34be1110a2192d0b3834b74ef469bd
mejango <mejango@protonmail.com>
2023-07-07 20:10:29 -0400

    3.1.1

--- src/MyDelegate.sol
+++ src/MyDelegate.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.20;
 // import {mulDiv} from '@prb/math/src/Common.sol';
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import {IJBDirectory} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBDirectory.sol";
-import {IJBFundingCycleDataSource} from
-    "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBFundingCycleDataSource.sol";
-import {IJBPayDelegate} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPayDelegate.sol";
+import {IJBFundingCycleDataSource3_1_1} from
+    "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBFundingCycleDataSource3_1_1.sol";
+import {IJBPayDelegate3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPayDelegate3_1_1.sol";
 import {IJBPaymentTerminal} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBPaymentTerminal.sol";
-import {IJBRedemptionDelegate} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBRedemptionDelegate.sol";
+import {IJBRedemptionDelegate3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBRedemptionDelegate3_1_1.sol";
 import {JBPayParamsData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBPayParamsData.sol";
-import {JBDidPayData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBDidPayData.sol";
-import {JBDidRedeemData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBDidRedeemData.sol";
+import {JBDidPayData3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBDidPayData3_1_1.sol";
+import {JBDidRedeemData3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBDidRedeemData3_1_1.sol";
 import {JBRedeemParamsData} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBRedeemParamsData.sol";
-import {JBPayDelegateAllocation} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBPayDelegateAllocation.sol";
-import {JBRedemptionDelegateAllocation} from
-    "@jbx-protocol/juice-contracts-v3/contracts/structs/JBRedemptionDelegateAllocation.sol";
+import {JBPayDelegateAllocation3_1_1} from "@jbx-protocol/juice-contracts-v3/contracts/structs/JBPayDelegateAllocation3_1_1.sol";
+import {JBRedemptionDelegateAllocation3_1_1} from
+    "@jbx-protocol/juice-contracts-v3/contracts/structs/JBRedemptionDelegateAllocation3_1_1.sol";
 import {DeployMyDelegateData} from "./structs/DeployMyDelegateData.sol";
 
 /// @notice A contract that is a Data Source, a Pay Delegate, and a Redemption Delegate.
 /// @dev This example implementation confines payments to an allow list.
-contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionDelegate {
+contract MyDelegate is IJBFundingCycleDataSource3_1_1, IJBPayDelegate3_1_1, IJBRedemptionDelegate3_1_1 {
     error INVALID_PAYMENT_EVENT(address caller, uint256 projectId, uint256 value);
     error INVALID_REDEMPTION_EVENT(address caller, uint256 projectId, uint256 value);
     error PAYER_NOT_ON_ALLOWLIST(address payer);
@@ -46,15 +46,15 @@ contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionD
         view
         virtual
         override
-        returns (uint256 weight, string memory memo, JBPayDelegateAllocation[] memory delegateAllocations)
+        returns (uint256 weight, string memory memo, JBPayDelegateAllocation3_1_1[] memory delegateAllocations)
     {
         // Forward the default weight received from the protocol.
         weight = _data.weight;
         // Forward the default memo received from the payer.
         memo = _data.memo;
         // Add `this` contract as a Pay Delegate so that it receives a `didPay` call. Don't send any funds to the delegate (keep all funds in the treasury).
-        delegateAllocations = new JBPayDelegateAllocation[](1);
-        delegateAllocations[0] = JBPayDelegateAllocation(this, 0);
+        delegateAllocations = new JBPayDelegateAllocation3_1_1[](1);
+        delegateAllocations[0] = JBPayDelegateAllocation3_1_1(this, 0, '');
     }
 
     /// @notice This function gets called when the project's token holders redeem.
@@ -68,15 +68,15 @@ contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionD
         view
         virtual
         override
-        returns (uint256 reclaimAmount, string memory memo, JBRedemptionDelegateAllocation[] memory delegateAllocations)
+        returns (uint256 reclaimAmount, string memory memo, JBRedemptionDelegateAllocation3_1_1[] memory delegateAllocations)
     {
         // Forward the default reclaimAmount received from the protocol.
         reclaimAmount = _data.reclaimAmount.value;
         // Forward the default memo received from the redeemer.
         memo = _data.memo;
         // Add `this` contract as a Redeem Delegate so that it receives a `didRedeem` call. Don't send any extra funds to the delegate.
-        delegateAllocations = new JBRedemptionDelegateAllocation[](1);
-        delegateAllocations[0] = JBRedemptionDelegateAllocation(this, 0);
+        delegateAllocations = new JBRedemptionDelegateAllocation3_1_1[](1);
+        delegateAllocations[0] = JBRedemptionDelegateAllocation3_1_1(this, 0, '');
     }
 
     /// @notice Indicates if this contract adheres to the specified interface.
@@ -84,8 +84,8 @@ contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionD
     /// @param _interfaceId The ID of the interface to check for adherence to.
     /// @return A flag indicating if the provided interface ID is supported.
     function supportsInterface(bytes4 _interfaceId) public view virtual override returns (bool) {
-        return _interfaceId == type(IJBFundingCycleDataSource).interfaceId
-            || _interfaceId == type(IJBPayDelegate).interfaceId || _interfaceId == type(IJBRedemptionDelegate).interfaceId;
+        return _interfaceId == type(IJBFundingCycleDataSource3_1_1).interfaceId
+            || _interfaceId == type(IJBPayDelegate3_1_1).interfaceId || _interfaceId == type(IJBRedemptionDelegate3_1_1).interfaceId;
     }
 
     constructor() {}
@@ -118,7 +118,7 @@ contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionD
     /// @dev Reverts if the calling contract is not one of the project's terminals.
     /// @dev This example implementation reverts if the payer isn't on the allow list.
     /// @param _data Standard Juicebox project payment data. See https://docs.juicebox.money/dev/api/data-structures/jbdidpaydata/.
-    function didPay(JBDidPayData calldata _data) external payable virtual override {
+    function didPay(JBDidPayData3_1_1 calldata _data) external payable virtual override {
         // Make sure the caller is a terminal of the project, and that the call is being made on behalf of an interaction with the correct project.
         if (
             msg.value != 0 || !directory.isTerminalOf(projectId, IJBPaymentTerminal(msg.sender))
@@ -132,7 +132,7 @@ contract MyDelegate is IJBFundingCycleDataSource, IJBPayDelegate, IJBRedemptionD
     /// @notice Received hook from the payment terminal after a redemption.
     /// @dev Reverts if the calling contract is not one of the project's terminals.
     /// @param _data Standard Juicebox project redemption data. See https://docs.juicebox.money/dev/api/data-structures/jbdidredeemdata/.
-    function didRedeem(JBDidRedeemData calldata _data) external payable virtual override {
+    function didRedeem(JBDidRedeemData3_1_1 calldata _data) external payable virtual override {
         // Make sure the caller is a terminal of the project, and that the call is being made on behalf of an interaction with the correct project.
         if (
             msg.value != 0 || !directory.isTerminalOf(projectId, IJBPaymentTerminal(msg.sender))
